8896ebe828981791cf9668b99a6565137e41b22b
ArthurKa <arthur.katruk@gmail.com>
2021-07-27 17:02:51 +0300

    feat: turn off no-useless-constructor rule

--- src/utils.js
+++ src/utils.js
@@ -223,6 +223,7 @@ module.exports = {
         'require-await': 'warn',
         '@typescript-eslint/type-annotation-spacing': 'warn',
         '@typescript-eslint/member-delimiter-style': 'warn',
+        'no-useless-constructor': 'off',
       },
       overrides: [
         {
